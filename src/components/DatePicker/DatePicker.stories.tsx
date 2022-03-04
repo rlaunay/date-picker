d9aaa726d0a5606a2140232e95a81ab7bf141677
Rémi Launay <relaunay@gmail.com>
2022-03-04 07:51:40 +0100

    remove example / improve setup

---
+++ src/components/DatePicker/DatePicker.stories.tsx
@@ -0,0 +1,10 @@
+import {DatePicker} from '.'
+
+export default {
+  title: 'Date Picker',
+  compenent: DatePicker
+}
+
+export const Default = (args) => {
+  return <DatePicker />
+}
--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useClickOutside } from '../../hooks/useClickOutside';
 
-import classes from './DatePicker.module.scss';
 import { checkDate, getDate, months, parseDate } from './utils';
+
+import './DatePicker.scss';
 
 type DatePickerProps = {
   className?: string | undefined;
@@ -12,7 +13,7 @@ type DatePickerProps = {
 }
 
 export const DatePicker: React.FC<DatePickerProps> = ({ className, style, onChange, value }) => {
-  const pickerRef = useRef(null);
+  const datePickerRef = useRef(null);
 
   const [datePickerValue, setDatePickerValue] = useState<string>(() => {
     if (value && checkDate(value)) {
@@ -44,6 +45,7 @@ export const DatePicker: React.FC<DatePickerProps> = ({ className, style, onChan
   });
 
   useEffect(() => {
+    console.log('ici')
     const date = `${year}-${month}-${day}`;
     setDatePickerValue(date)
 
@@ -52,20 +54,20 @@ export const DatePicker: React.FC<DatePickerProps> = ({ className, style, onChan
     }
   }, [day, month, year])
 
-  useClickOutside(pickerRef, () => {
+  useClickOutside(datePickerRef, () => {
     setIsOpen(false)
   })
   
   return (
-    <span className={classes.container} >
+    <span className="container" ref={datePickerRef} >
       <input
         type="text" 
         style={style} 
         className={className}
         onFocus={() => setIsOpen(true)} 
-        value={value} 
+        value={datePickerValue} 
       />
-      {isOpen && <div className={classes.picker} ref={pickerRef} >
+      {isOpen && <div className="picker" >
         <div>
           {months[month - 1]} {year}
         </div>
@@ -82,7 +84,60 @@ export const DatePicker: React.FC<DatePickerProps> = ({ className, style, onChan
             </tr>
           </thead>
           <tbody>
-
+            <tr>
+              <td>30</td>
+              <td>31</td>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+            </tr>
+            <tr>
+              <td>6</td>
+              <td>7</td>
+              <td>8</td>
+              <td>9</td>
+              <td>10</td>
+              <td>11</td>
+              <td>12</td>
+            </tr>
+            <tr>
+              <td>13</td>
+              <td>14</td>
+              <td>15</td>
+              <td>16</td>
+              <td>17</td>
+              <td>18</td>
+              <td>19</td>
+            </tr>
+            <tr>
+              <td>20</td>
+              <td>21</td>
+              <td>22</td>
+              <td>23</td>
+              <td>24</td>
+              <td>25</td>
+              <td>26</td>
+            </tr>
+            <tr>
+              <td>27</td>
+              <td>28</td>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+            </tr>
+            <tr>
+              <td>6</td>
+              <td>7</td>
+              <td>8</td>
+              <td>9</td>
+              <td>10</td>
+              <td>11</td>
+              <td>12</td>
+            </tr>
           </tbody>
         </table>
       </div>}
